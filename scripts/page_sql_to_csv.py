b4bdeb99b033645971a35473766907c8fe732c65
Jakob1010 <aichinger.j.k@gmail.com>
2022-04-04 22:26:13 +0200

    only include page records with correct number of attributes
    todo: inspect broken records

--- scripts/page_sql_to_csv.py
+++ scripts/page_sql_to_csv.py
@@ -6,10 +6,11 @@ Convert all insert statements from 'dewiki-20220320-page.sql' to csv schema.
 FILE_NAME = "dewiki-20220320-page"
 PATH = "resources_tmp/"
 INPUT_FILE = open(PATH + FILE_NAME + '.sql')
+PAGE_RECORD_ATTRIBUTES = 13
 
 out = ""
 insert_statement_found = False
-page_object_found = False
+page_record_found = False
 
 for i, line in enumerate(INPUT_FILE):
 
@@ -20,16 +21,17 @@ for i, line in enumerate(INPUT_FILE):
     if insert_statement_found:
         # iterate over single line
         for j, character in enumerate(line):
+            # start of object
             if character == '(':
-                page_object_found = True
+                insert_statement_found = True
+            # end of object
             elif character == ')' and line[j+1] == ',':
-                # ignore semicolon in between two objects
-                print(out)
-                page_object_found = False
+
+                n = len(out.split(','))
+                if n == PAGE_RECORD_ATTRIBUTES:
+                    print(out)
+
+                insert_statement_found = False
                 out = ""
-            elif page_object_found:
+            elif insert_statement_found:
                 out += character
-
-
-
-
