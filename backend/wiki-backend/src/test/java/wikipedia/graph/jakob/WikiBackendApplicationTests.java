987b6398e66e31fd18794bc5f548bda03f5ffeee
Jakob1010 <aichinger.j.k@gmail.com>
2022-04-05 22:50:13 +0200

    added spring boot template for backend

---
+++ backend/wiki-backend/src/test/java/wikipedia/graph/jakob/WikiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package wikipedia.graph.jakob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
